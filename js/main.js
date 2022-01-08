b83b3a37788a3da297901a1ac707154d7d2b437e
JaredLarios <lar21038@byui.edu>
2022-01-08 01:56:53 -0600

    JS integration - update date

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let copyRight = document.querySelector("#copy-right");
+copyRight.innerHTML = "&copy 2022 .:|:. E. Jared Larios.:|:.";
+
+let updateDate = document.lastModified;
+document.write(`<p>Last Updated: ${updateDate}</p>`)
+
+console.log(copyRight);
