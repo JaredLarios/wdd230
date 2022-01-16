6cde81e97b7ca23c46266612a6701f239172e66d
JaredLarios <lar21038@byui.edu>
2022-01-15 22:08:08 -0600

    uploaded the lesson 02

---
+++ lesson2/js/script.js
@@ -0,0 +1,7 @@
+
+const updates = document.getElementById("update");
+let lastUpdate = document.lastModified;
+
+updates.innerHTML = `&copy 2022 | Jared Larios | Last Updated: ${lastUpdate}`;
+
+console.log(copyRight);
