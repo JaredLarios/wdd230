98b979bb515c61ed35cadd4829e517f6bc7a5f5d
JaredLarios <lar21038@byui.edu>
2022-01-28 21:21:50 -0600

    chore(fa-bars): responsive btn

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,7 +1,10 @@
 const menuBars = document.querySelector(".menu-icon")
 const menu = document.querySelector(".menu")
+const row = document.querySelector(".fa-bars")
 
 
-menuBars.addEventListener('click', () => 
-{menu.classList.toggle('responsive')}, false);
+menuBars.addEventListener('click', () => {
+    menu.classList.toggle('responsive');
+    row.classList.toggle('fa-chevron-up');
+}, false);
 
