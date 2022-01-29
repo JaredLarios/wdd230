8bb98fc73ecbdb1109798ff33073b27141685052
JaredLarios <lar21038@byui.edu>
2022-01-28 21:18:37 -0600

    feat: s res-menu & cards

---
+++ chamber/js/script.js
@@ -0,0 +1,7 @@
+const menuBars = document.querySelector(".menu-icon")
+const menu = document.querySelector(".menu")
+
+
+menuBars.addEventListener('click', () => 
+{menu.classList.toggle('responsive')}, false);
+
