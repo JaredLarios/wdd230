6a140922f6f96b5f223d10acff2c9697364b44c1
JaredLarios <lar21038@byui.edu>
2022-01-29 23:46:29 -0600

    chore(foot): info

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,7 +1,8 @@
 const menuBars = document.querySelector(".menu-icon")
 const menu = document.querySelector(".menu")
 const row = document.querySelector(".fa-bars")
-const foot = document.querySelector("footer")
+const foot = document.getElementById("mod")
+const infoFoot = document.getElementById("info")
 
 
 menuBars.addEventListener('click', () => {
@@ -16,5 +17,15 @@ window.onresize = () => {
     };
 };
 
+const info ={
+    myName : 'GT Chamber',
+    myAdd : '14 st 19-35, Guatemala City',
+    myEmail : 'info@gtchamber.org',
+    myPhone : '(+502)43435151',
+    logo: '<img class="logo" src="images/logo.jpg" alt="logo">'
+}
+
+infoFoot.innerHTML = `${info.logo} <p>${info.myName}</p> <p>${info.myAdd}</p> <p>${info.myEmail}</p> <p>${info.myPhone}</p>`;
+
 const lastMod = (year, updateDate) => `&copy ${year} | GT Chamber | E. Jared Larios | WDD 230 Project | Last Modification: ${updateDate}`;
-foot.innerText = lastMod(new Date().getFullYear(), document.lastModified);
+foot.innerHTML = `<p>${lastMod(new Date().getFullYear(), document.lastModified)}</p>`;
