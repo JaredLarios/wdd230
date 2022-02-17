7558ffbda8eedf448d5b96efa8e53913b9cab8ee
JaredLarios <lar21038@byui.edu>
2022-02-16 20:56:32 -0600

    fix(popup): days and add more coments

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,19 +1,24 @@
 ///  modelus 
 import weather from "./windchill.js"
 
-/// footer
+/// selctors
 const menuBars = document.querySelector(".menu-icon")
 const menu = document.querySelector(".menu")
 const row = document.querySelector(".fa-bars")
 const foot = document.getElementById("mod")
 const infoFoot = document.getElementById("info")
 
+// -----------------------------------------------------------------------------------
 
+/* menu responsive */
 menuBars.addEventListener('click', () => {
     menu.classList.toggle('responsive');
     row.classList.toggle('fa-chevron-up');
 }, false);
 
+// ---------------------------------------------------------------------------
+
+/* remove menu responsive*/
 window.onresize = () => {
     if (window.innerWidth > 700){
         menu.classList.remove('responsive');
@@ -21,6 +26,7 @@ window.onresize = () => {
     };
 };
 
+// Chamber's info footer
 const info ={
     myName : 'GT Chamber',
     myAdd : '14 st 19-35, Guatemala City',
@@ -34,6 +40,7 @@ infoFoot.innerHTML = `${info.logo} <div><h2>${info.myName}</h2> <p>${info.myAdd}
 const lastMod = (year, updateDate) => `&copy ${year} | GT Chamber | E. Jared Larios | WDD 230 Project | Last Modification: ${updateDate}`;
 foot.innerHTML = `<p>${lastMod(new Date().getFullYear(), document.lastModified)}</p>`;
 
+// -------------------------------------------------------------------------------------------------------------
 // date 
 const datefield = document.querySelector(".date");
 const now = new Date();
@@ -44,8 +51,11 @@ Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
+//-------------------------------------------------------------------------------------
 
-/*
+/* Slide Show*/
+/* Disabled for Week 7 review
+
 window.addEventListener('load', () => {
     console.log('loaded items');
     const images = ['images/chamber.jpg','images/chamber-2.jpg','images/chamber-3.jpg', 'images/wdd230-org.jpg'];
@@ -71,12 +81,14 @@ window.addEventListener('load', () => {
     setInterval(slideShow, 2 * 3600);
 });
 */
-// Pop Up
+
+// ------------------------------------------------------------------------------------
+// Pop Up Banner 
 
 
 console.log(now.getDay())
 
-if (now.getDay() === 2 || now.getDay() == 3 ) {
+if (now.getDay() === 1 || now.getDay() == 2 ) {
     datefield.innerHTML += 
     `<p><a href='#'>
     🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m
@@ -84,6 +96,8 @@ if (now.getDay() === 2 || now.getDay() == 3 ) {
     menu.classList.toggle('responsive-2');
     document.querySelector('.pop').classList.toggle('space')
 }
+
+// ----------------------------------------------------------------------------------------------
 
 /* Weather */
 
