6bac61392855ac279bc8464aa37cbe797a3d23b1
JaredLarios <lar21038@byui.edu>
2022-02-17 19:50:01 -0600

    feat(preload): add preolad funct

--- lesson7/js/main.js
+++ lesson7/js/main.js
@@ -1,1 +1,26 @@
 /*  Lazzzy Loading */
+
+const iamges = document.querySelectorAll('[data-src]');
+
+function preLoadImage(img) {
+    const src = img.getAttibute("data-src");
+    if (!src) {
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions = {};
+
+const imgObserver  = new IntersectionObserver(
+    (entries, imgObserver) => {
+        entries.forEach( entry => {
+            if (!FileSystemEntry.isIntersection) {
+                return;
+            } else {
+                preLoadImage(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        })
+    }, imgOptions);
