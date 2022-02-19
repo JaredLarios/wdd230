660b4d42a5537241033a78490b52523489c12350
JaredLarios <lar21038@byui.edu>
2022-02-19 14:55:10 -0600

    chore(js): lazzy loading and design update

---
+++ chamber/discover/js/script.js
@@ -0,0 +1,34 @@
+/*  Lazzzy Loading */
+
+const iamges = document.querySelectorAll('[data-src]');
+
+function preloadImage(img) {
+    const srcs = img.getAttribute("data-src");
+    if (!srcs) {
+        return;
+    }
+
+    img.src = srcs;
+    img.onload = () => {img.removeAttribute('data-src')};
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px 500px 0px',
+};
+
+const imgObserver  = new IntersectionObserver(
+    (entries, imgObserver) => {
+        entries.forEach( entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                imgObserver.unobserve(entry.target);
+            }
+        })
+    }, imgOptions);
+
+iamges.forEach(image => {
+    imgObserver.observe(image);
+})
