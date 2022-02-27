41d72d48712e1a74e1acba073b6fa829ec713f6e
Jared Larios <lar21038@byui.edu>
2022-02-26 23:50:06 -0600

    chore: add validation of Position

--- chamber/join/js/script.js
+++ chamber/join/js/script.js
@@ -1,13 +1,33 @@
-let myBusiPoistion = document.querySelector('.regulation')
-
-if (regex.test(myBusiPoistion).value) {
-    myBusiPoistion.toggleClass('valid');
-}
-else {
-    myBusiPoistion.toggleClass('invalid');
-}
+const myBusiPoistion = document.querySelector('.regulation')
+const errMessage = document.querySelector('#posErr')
+const dateEntry = document.querySelector('#date')
+const btn = document.querySelector('.submitBtn')
 
 
-myBusiPoistion.onkeydown = function() {
-    const regex = /\w\s{7,}/;
+/* ------   Date Time    ------*/
+
+let date = new Date
+console.log(date)
+
+dateEntry.textContent = date;
+
+
+/* ------   Validation    ------*/
+function validate () {
+    const regex = /(\w+|\s+){7,}/
+
+    const val = myBusiPoistion.value.match(regex)
+
+    console.log(val)
+    
+    if (val == null) {
+        myBusiPoistion.classList.toggle('valid');
+        errMessage.innerHTML = 'The postion must have at least 7 character';
+        console.log(errMessage)
+    }
+    else {
+        myBusiPoistion.classList.toggle('invalid');
+    }
 }
+
+btn.addEventListener('click', validate)
