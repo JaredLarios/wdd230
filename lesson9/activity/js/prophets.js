fd4d8f4aae84e1ddd1ecb9821fadac0093ce3976
Jared Larios <lar21038@byui.edu>
2022-03-03 17:22:34 -0600

    chore(span): add b-place and date

--- lesson9/activity/js/prophets.js
+++ lesson9/activity/js/prophets.js
@@ -16,6 +16,7 @@ fetch(requestURL)
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let portrait = document.createElement('img');
+    let birthInfo = document.createElement('p');
     let numberPresident = '';
     numberPresident = prophet.order;
 
@@ -23,6 +24,8 @@ fetch(requestURL)
   
     // Change the textContent property of the h2 element to contain the prophet's full name
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
+    birthInfo.innerHTML = `<span>Date of Birth: ${prophet.birthdate}</span> <br><span>Place of Birth: ${prophet.birthplace}</span>`
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
@@ -31,6 +34,7 @@ fetch(requestURL)
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
+    card.appendChild(birthInfo);
     card.appendChild(portrait);
   
     // Add/append the existing HTML div with the cards class with the section(card)
