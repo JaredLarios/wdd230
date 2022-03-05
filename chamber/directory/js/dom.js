cf25e65540c90266115a1960a2af9b3dbdf04e3b
Jared Larios <lar21038@byui.edu>
2022-03-05 11:09:16 -0600

    chore: responsive default view

--- chamber/directory/js/dom.js
+++ chamber/directory/js/dom.js
@@ -5,31 +5,44 @@ const myDirectory = {
     convertJson: (file) => file.json(),
     
     test: (data) => console.table(data),
+    
+    clear: ()=> dom.innerHTML='',
 
     displayInfoList: (data) => {
-       let row =  document.createElement('ul');
 
-       row.innerHTML = `<li>${data.name}</li>
+       dom.innerHTML += `
+                    <ul>
+                        <li>${data.name}</li>
                         <li>${data.address}</li>
                         <li>${data.phone}</li>
-                        <li>${data.url}</li>                  
+                        <li>${data.url}</li>        
+                    </ul>          
        `
-
-       dom.appendChild(row);
     },
 
     displayInfoGallery: (data) => {
-        let row =  document.createElement('div');
+        let card =  document.createElement('div');
  
-        row.innerHTML = `<img src='${data.photo}' alt='${data.name}'>
+        dom.innerHTML += `
+                    <div>
+                         <img src='${data.photo}' alt='${data.name}'>
                          <span>${data.address}</span><br>
                          <span>${data.phone}</span><br>
-                         <span>${data.url}</span><br>                  
+                         <span>${data.url}</span><br> 
+                    </div>                 
         `
- 
-        dom.appendChild(row);
      }
 
+    /*defaultDisplay: (small, medium, large, business) =>{
+        if (small == true || large == true) {
+            myDirectory.clear();
+            business.forEach(e => myDirectory.displayInfoGallery(e))
+        }
+        else if (medium == true) {
+                myDirectory.clear();
+                business.forEach(e => myDirectory.displayInfoList(e))
+        }
+    }*/
 }
 
 export default myDirectory
--- chamber/directory/js/script.js
+++ chamber/directory/js/script.js
@@ -1,5 +1,10 @@
 import myDirectory from "./dom.js";
 
+/*--- icons on the HTML ---*/
+const list = document.querySelector('.fa-th-list')
+const grid = document.querySelector('.fa-th')
+const medium = window.matchMedia("(min-width: 34.5em) and (max-width: 47.7em)")
+
 /*--- Json file from github ---*/
 const url = "https://jaredlarios.github.io/wdd230/chamber/directory/data/directory.json"
 
@@ -11,6 +16,23 @@ fetch(url)
     (file) =>{
         myDirectory.test
         const business = file['directory']
-        business.forEach(e => myDirectory.displayInfoList(e))
+
+        if (medium.matches) {
+            myDirectory.clear();
+            business.forEach(e => myDirectory.displayInfoList(e))
+        }
+        else {
+            myDirectory.clear();
+            business.forEach(e => myDirectory.displayInfoGallery(e))
+        }
+
+        list.addEventListener('click',() =>{
+            myDirectory.clear();
+            business.forEach(e => myDirectory.displayInfoList(e))
+        })
+        grid.addEventListener('click', ()=>{
+            myDirectory.clear();
+            business.forEach(e => myDirectory.displayInfoGallery(e))
+        })
     } 
 )
