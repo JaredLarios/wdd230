842af9b7c79982513b592446a82dc95c49a46d09
Jared Larios <lar21038@byui.edu>
2022-03-10 09:13:57 -0600

    chore: api wather init

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,9 @@
+const url = 'api.openweathermap.org/data/2.5/weather?q=London,uk&appid=1fc2f1cda26c5b2888cee907631c3fa7'
+
+fetch(url)
+    .then( response => response.json())
+    .then(
+        jsObject => {
+            console.log(jsObject)
+        }
+    )
