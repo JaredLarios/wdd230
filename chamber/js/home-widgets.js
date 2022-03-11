7fc094ccb33e4ebc3e641911f7e0699c507b55ce
Jared Larios <lar21038@byui.edu>
2022-03-11 12:37:49 -0600

    fix: fetch error

--- chamber/js/home-widgets.js
+++ chamber/js/home-widgets.js
@@ -1,32 +1,4 @@
 const url = 'http://api.openweathermap.org/data/2.5/weather?q=Guatemala&APPID=7f554438855760396460305b4e229983'
-
-/* Slide Show*/
-
-window.addEventListener('load', () => {
-    console.log('loaded items');
-    const images = ['images/chamber.jpg', 'images/chamber-2.jpg', 'images/chamber-3.jpg', 'images/wdd230-org.jpg'];
-    const responsive = [
-        'images/chamber200.jpg 200w, images/chamber500.jpg 500w, images/chamber800.jpg 800w',
-        'images/chamber-2-200.jpg 200w, images/chamber-2-500.jpg 500w, images/chamber-2-800.jpg 800w',
-        'images/chamber-3-200.jpg 200w, images/chamber-3-500.jpg 500w, images/chamber-3-800.jpg 800w',
-        'images/wdd230-org200.jpg 200w, images/wdd230-org500.jpg 500w, images/wdd230-org800.jpg 800w'
-    ];
-    let indexImage = 0;
-
-    const slideShow = () => {
-
-        document.querySelector('#slide').setAttribute('src', images[indexImage])
-        document.querySelector('#slide').setAttribute('srcset', responsive[indexImage])
-
-        if (indexImage < 3) {
-            indexImage++;
-        } else {
-            indexImage = 0;
-        }
-    }
-    setInterval(slideShow, 2 * 3600);
-});
-
 /* wind chill */
 
 const dSel = (e) => document.getElementById(e);
@@ -71,3 +43,30 @@ fetch(url)
             }
         }
     )
+
+/* Slide Show*/
+
+window.addEventListener('load', () => {
+    console.log('loaded items');
+    const images = ['images/chamber.jpg', 'images/chamber-2.jpg', 'images/chamber-3.jpg', 'images/wdd230-org.jpg'];
+    const responsive = [
+        'images/chamber200.jpg 200w, images/chamber500.jpg 500w, images/chamber800.jpg 800w',
+        'images/chamber-2-200.jpg 200w, images/chamber-2-500.jpg 500w, images/chamber-2-800.jpg 800w',
+        'images/chamber-3-200.jpg 200w, images/chamber-3-500.jpg 500w, images/chamber-3-800.jpg 800w',
+        'images/wdd230-org200.jpg 200w, images/wdd230-org500.jpg 500w, images/wdd230-org800.jpg 800w'
+    ];
+    let indexImage = 0;
+
+    const slideShow = () => {
+
+        document.querySelector('#slide').setAttribute('src', images[indexImage])
+        document.querySelector('#slide').setAttribute('srcset', responsive[indexImage])
+
+        if (indexImage < 3) {
+            indexImage++;
+        } else {
+            indexImage = 0;
+        }
+    }
+    setInterval(slideShow, 2 * 3600);
+});
