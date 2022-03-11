6b3fa2f09c24427488d34c7651fc0b0a0eed625b
Jared Larios <lar21038@byui.edu>
2022-03-11 08:59:10 -0600

    chore: made it prettier

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,14 +1,14 @@
 const url = 'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&APPID=1fc2f1cda26c5b2888cee907631c3fa7'
 
 fetch(url)
-    .then( response => response.json())
+    .then(response => response.json())
     .then(
         jsObject => {
             console.log(jsObject)
             document.querySelector('#current-temp').textContent = jsObject.main.temp;
             document.querySelector('#max-temp').textContent = jsObject.main.temp_max;
             document.querySelector('#min-temp').textContent = jsObject.main.temp_min;
-            const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+            const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
             const desc = jsObject.weather[0].description;
             document.querySelector('#icon-src').textContent = iconsrc;
             document.querySelector('#weathericon').setAttribute('src', iconsrc);
