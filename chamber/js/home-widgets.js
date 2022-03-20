3e99a5ac09af12e935b4c9816ba5498be24123c7
Jared Larios <lar21038@byui.edu>
2022-03-19 22:11:43 -0600

    fix: slide show

--- chamber/js/home-widgets.js
+++ chamber/js/home-widgets.js
@@ -45,31 +45,29 @@ fetch(url)
     )
     .then(
         () => {
-            /* Slide Show*/
+            /* Slide Show*/        
+            console.log('loaded items');
+            const images = ['images/chamber.jpg', 'images/chamber-2.jpg', 'images/chamber-3.jpg', 'images/wdd230-org.jpg'];
+            const responsive = [
+                'images/chamber200.jpg 200w, images/chamber500.jpg 500w, images/chamber800.jpg 800w',
+                'images/chamber-2-200.jpg 200w, images/chamber-2-500.jpg 500w, images/chamber-2-800.jpg 800w',
+                'images/chamber-3-200.jpg 200w, images/chamber-3-500.jpg 500w, images/chamber-3-800.jpg 800w',
+                'images/wdd230-org200.jpg 200w, images/wdd230-org500.jpg 500w, images/wdd230-org800.jpg 800w'
+            ];
+            let indexImage = 0;
 
-            window.addEventListener('load', () => {
-                console.log('loaded items');
-                const images = ['images/chamber.jpg', 'images/chamber-2.jpg', 'images/chamber-3.jpg', 'images/wdd230-org.jpg'];
-                const responsive = [
-                    'images/chamber200.jpg 200w, images/chamber500.jpg 500w, images/chamber800.jpg 800w',
-                    'images/chamber-2-200.jpg 200w, images/chamber-2-500.jpg 500w, images/chamber-2-800.jpg 800w',
-                    'images/chamber-3-200.jpg 200w, images/chamber-3-500.jpg 500w, images/chamber-3-800.jpg 800w',
-                    'images/wdd230-org200.jpg 200w, images/wdd230-org500.jpg 500w, images/wdd230-org800.jpg 800w'
-                ];
-                let indexImage = 0;
+            const slideShow = () => {
 
-                const slideShow = () => {
+                document.querySelector('#slide').setAttribute('src', images[indexImage])
+                document.querySelector('#slide').setAttribute('srcset', responsive[indexImage])
 
-                    document.querySelector('#slide').setAttribute('src', images[indexImage])
-                    document.querySelector('#slide').setAttribute('srcset', responsive[indexImage])
-
-                    if (indexImage < 3) {
-                        indexImage++;
-                    } else {
-                        indexImage = 0;
-                    }
+                if (indexImage < 3) {
+                    indexImage++;
+                } else {
+                    indexImage = 0;
                 }
-                setInterval(slideShow, 2 * 3600);
-            });
+            }
+            setInterval(slideShow, 2 * 3600);
+            
         }
     )
