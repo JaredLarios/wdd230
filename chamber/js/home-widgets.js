79a738d0309b5b82880bc31abec24a684595e415
Jared Larios <lar21038@byui.edu>
2022-03-20 00:23:43 -0600

    chore: add responsive icons

--- chamber/js/home-widgets.js
+++ chamber/js/home-widgets.js
@@ -1,6 +1,6 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?q=Guatemala&APPID=7f554438855760396460305b4e229983'
+const url2 = 'https://jaredlarios.github.io/wdd230/chamber/directory/data/directory.json'
 /* wind chill */
-
 const dSel = (e) => document.getElementById(e);
 const windChill = (temp, speed) => {
     return (35.74 + 0.6215 * temp) - 35.75 * (speed ** 0.16) + 0.4275 * (temp * speed ** 0.16);
@@ -45,7 +45,7 @@ fetch(url)
     )
     .then(
         () => {
-            /* Slide Show*/        
+            /* Slide Show*/
             console.log('loaded items');
             const images = ['images/chamber.jpg', 'images/chamber-2.jpg', 'images/chamber-3.jpg', 'images/wdd230-org.jpg'];
             const responsive = [
@@ -68,6 +68,40 @@ fetch(url)
                 }
             }
             setInterval(slideShow, 2 * 3600);
+
+        }
+    )
+
+fetch(url2)
+    .then((response) => response.json())
+    .then(
+        (obj) => {
+            console.log(obj);
+
             
+            
+            /*get a random number*/
+            const len = obj.directory.length
+            let randChoice1 = Math.floor(Math.random() * len);
+            console.log(randChoice1)
+            let randChoice2 = Math.floor(Math.random() * len);
+             
+            /* Fill stages */
+
+            const header1 = document.querySelector(".stage1h")
+            const info1 = document.querySelector(".stage1p")
+            const img1 = document.querySelector(".stage1-img")
+            
+            const header2 = document.querySelector(".stage2h")
+            const info2 = document.querySelector(".stage2p")
+            const img2 = document.querySelector(".stage2-img")
+
+            header1.textContent = obj.directory[randChoice1].name
+            info1.innerHTML = `${obj.directory[randChoice1].address} <br> ${obj.directory[randChoice1].phone}`
+            img1.setAttribute("src", obj.directory[randChoice1].photo)
+
+            header2.textContent = obj.directory[randChoice2].name
+            info2.innerHTML = `${obj.directory[randChoice2].address} <br> ${obj.directory[randChoice2].phone}`
+            img2.setAttribute("src", obj.directory[randChoice2].photo)
         }
     )
