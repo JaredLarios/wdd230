ca51c0f3eaed5a953a0a191c3c1900fb61685092
Jared Larios <lar21038@byui.edu>
2022-03-31 10:35:54 -0600

    chore: add lazzy loading of images

--- tis/js/base.js
+++ tis/js/base.js
@@ -4,10 +4,10 @@ const menu = document.querySelector(".menu")
 
 const Base = {
     scroll: () => {
-        window.addEventListener("scroll" , () => {
-            menuBar.classList.toggle("show-color", window.scrollY > 200) 
+        window.addEventListener("scroll", () => {
+            menuBar.classList.toggle("show-color", window.scrollY > 200)
         })
-    }, 
+    },
     menu: () => {
         const menuBars = document.querySelector(".menu-bars")
 
@@ -16,6 +16,42 @@ const Base = {
                 menuBar.classList.toggle("show-color")
             }
             menu.classList.toggle("show-menu")
+        })
+    },
+    lazzy: () => {
+        const images = document.querySelectorAll('[data-src]');
+
+        function preloadImage(img) {
+            const srcs = img.getAttribute("data-src");
+            const srcsets = img.getAttribute("data-srcset");
+            if (!srcs || !srcsets) {
+                return;
+            }
+
+            img.src = srcs;
+            img.srcset = srcsets;
+            img.onload = () => { img.removeAttribute('data-src'); img.removeAttribute('data-srcset') };
+        }
+
+        const imgOptions = {
+            threshold: 0,
+            rootMargin: '0px 0px 400px 0px',
+        };
+
+        const imgObserver = new IntersectionObserver(
+            (entries, imgObserver) => {
+                entries.forEach(entry => {
+                    if (!entry.isIntersecting) {
+                        return;
+                    } else {
+                        preloadImage(entry.target);
+                        imgObserver.unobserve(entry.target);
+                    }
+                })
+            }, imgOptions);
+
+            images.forEach(image => {
+            imgObserver.observe(image);
         })
     }
 }
--- tis/js/script.js
+++ tis/js/script.js
@@ -2,3 +2,4 @@ import Base from "./base.js"
 
 Base.scroll()
 Base.menu()
+Base.lazzy()
